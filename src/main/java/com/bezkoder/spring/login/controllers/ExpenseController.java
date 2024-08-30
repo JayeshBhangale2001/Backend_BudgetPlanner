faaa1aaedd9081acccfbea26ad1845631209f633
JayeshBhangale2001 <96966759+JayeshBhangale2001@users.noreply.github.com>
2024-08-31 00:18:36 +0530

    BudgetPlanner_Backend_#2  Expense Statistics

--- src/main/java/com/bezkoder/spring/login/controllers/ExpenseController.java
+++ src/main/java/com/bezkoder/spring/login/controllers/ExpenseController.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.bezkoder.spring.login.dto.ExpenseDTO;
+import com.bezkoder.spring.login.dto.ExpenseStatisticsDTO;
 import com.bezkoder.spring.login.models.Expense;
 import com.bezkoder.spring.login.security.services.UserDetailsImpl;
 import com.bezkoder.spring.login.services.ExpenseService;
@@ -11,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
+import java.sql.Date;
 import java.util.List;
 
 @CrossOrigin(origins = {"http://localhost:4200", "https://financemanagement-dac21.web.app"}, maxAge = 3600, allowCredentials = "true")
@@ -63,5 +65,23 @@ public class ExpenseController {
     @DeleteMapping("/{id}")
     public void deleteExpense(@PathVariable Long id) {
         expenseService.deleteExpense(id);
+    }
+    
+    @GetMapping("/statistics")
+    public ExpenseStatisticsDTO getExpenseStatistics(
+            @RequestParam String timeline,
+            @RequestParam Date startDate,
+            @RequestParam Date endDate,
+            Authentication authentication) {
+        UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
+        Long userId = userDetails.getId();
+
+        logger.info("Fetching expense statistics for userId: {}, timeline: {}, startDate: {}, endDate: {}", userId, timeline, startDate, endDate);
+
+        ExpenseStatisticsDTO statistics = expenseService.getExpenseStatistics(userId, startDate, endDate, timeline);
+
+        logger.info("Expense statistics fetched: {}", statistics);
+
+        return statistics;
     }
 }
